a969c926bb8f5719ea24038f0fd7f6b2f7d0d011
Joshua Crane <josh@bintree.io>
2025-06-05 17:41:04 +0900

    added ami for build out of ec2 instances

--- jc-terransible/networking.tf
+++ jc-terransible/networking.tf
@@ -48,9 +48,9 @@ resource "aws_default_route_table" "jc_private_rt" {
 }
 
 resource "aws_subnet" "jc_public_subnet" {
-    count = 2
+    count = length(local.azs)
     vpc_id = aws_vpc.jc_vpc.id
-    cidr_block = var.public_cidrs[count.index]
+    cidr_block = cidrsubnet(var.vpc_cidr, 8, count.index)
     map_public_ip_on_launch = true
     availability_zone = local.azs[count.index]
 
@@ -60,9 +60,9 @@ resource "aws_subnet" "jc_public_subnet" {
 }
 
 resource "aws_subnet" "jc_private_subnet" {
-    count = 2
+    count = length(local.azs)
     vpc_id = aws_vpc.jc_vpc.id
-    cidr_block = var.private_cidrs[count.index]
+    cidr_block = cidrsubnet(var.vpc_cidr, 8, length(local.azs) + count.index)
     map_public_ip_on_launch = false
     availability_zone = local.azs[count.index] 
 
@@ -70,4 +70,35 @@ resource "aws_subnet" "jc_private_subnet" {
         Name = "jc-private-${count.index + 1}"
         
     }      
+}
+
+resource "aws_route_table_association" "jc_public_assoc" {
+    count = length(local.azs)
+    subnet_id = aws_subnet.jc_public_subnet.*.id[count.index]
+    route_table_id = aws_route_table.jc_public_rt.id
+}
+
+resource "aws_security_group" "jc_sg" {
+    name = "public_sg"
+    description = "Security group for public instances"
+    vpc_id = aws_vpc.jc_vpc.id
+
+}
+
+resource "aws_security_group_rule" "ingress_all" {
+    type = "ingress"
+    from_port = 0
+    to_port = 65535
+    protocol = "-1"
+    cidr_blocks = [var.access_ip]
+    security_group_id = aws_security_group.jc_sg.id
+}
+
+resource "aws_security_group_rule" "egress_all" {
+    type = "ingress"
+    from_port = 0
+    to_port = 65535
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    security_group_id = aws_security_group.jc_sg.id
 }
