af2215bf26109cda18cd891c05eb705ed967c0c0
Joshua Crane <josh@bintree.io>
2025-06-08 21:01:44 +0900

    removing files

--- jc-terransible/compute.tf
+++ jc-terransible/compute.tf
@@ -24,7 +24,7 @@ resource "aws_instance" "jc_main" {
   key_name               = aws_key_pair.jc_auth.key_name
   vpc_security_group_ids = [aws_security_group.jc_sg.id]
   subnet_id              = aws_subnet.jc_public_subnet[count.index].id
-  user_data              = templatefile("./main-userdata.tpl", { new_hostname = "jc-main-${random_id.jc_node_id[count.index].dec}" })
+  #user_data              = templatefile("./main-userdata.tpl", { new_hostname = "jc-main-${random_id.jc_node_id[count.index].dec}" })
   root_block_device {
     volume_size = var.main_vol_size
 
@@ -33,21 +33,32 @@ resource "aws_instance" "jc_main" {
     Name = "jc-main-${random_id.jc_node_id[count.index].dec}"
   }
 
-#  provisioner "local-exec" {
-#    command = "printf \n${self.public_ip} >> aws_hosts"
-#  }
+  provisioner "local-exec" {
+   command = "echo ${self.public_ip} >> aws_hosts"
+}
+  provisioner "local-exec" {
+    when = destroy 
+    command = "sed -i '/^[0-9]/d' aws_hosts"
+  }
 }
 
-resource "null_resource" "grafana_update" {
-  count = var.main_instance_count
-  provisioner "remote-exec" {
-    inline = ["sudo apt upgrade -y grafana && touch upgrade.log && echo 'I updated grafana' >> upgrade.log"]
-    connection {
-      type        = "ssh"
-      user        = "ubuntu"
-      private_key = file("jc-terransible")
-      host        = aws_instance.jc_main[count.index].public_ip
-    }
-  }
+#resource "null_resource" "grafana_update" {
+#  count = var.main_instance_count
+#  provisioner "remote-exec" {
+#    inline = ["sudo apt upgrade -y grafana && touch upgrade.log && echo 'I updated grafana' >> upgrade.log"]
+#    connection {
+#      type        = "ssh"
+#      user        = "ubuntu"
+#      private_key = file("jc-terransible")
+#      host        = aws_instance.jc_main[count.index].public_ip
+#    }
+#  }
+#
+#}
 
+resource "null_resource" "grafana_install" {
+    depends_on = [aws_instance.jc_main]
+    provisioner "local-exec" {
+        command = "ansible-playbook -i aws_hosts --key-file jc-terransible playbooks/grafana.yml"
+    }
 }
